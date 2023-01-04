4b303e7366a359a1e456b899e2d9036c49774834
alex-jones <abjhoops@gmail.com>
2023-01-03 20:49:27 -0700

    complete lab

---
+++ data/students-data.js
@@ -0,0 +1,9 @@
+const students = [
+    {name: 'Alex Jones', age: 25, enrolled: true, _id: 1233},
+    {name: 'John Doe', age: 36, enrolled: true, _id: 1234},
+    {name: 'Jane Dear', age: 68, enrolled: false, _id: 1235}
+]
+
+export {
+    students
+}
